d63d021675a40ed910ad0c9bcb179317c43e98d0
kelwilson <kelwilsonachienu@gmail.com>
2022-06-18 23:15:49 +0800

    Add an event listener for form validation and error messages

--- form-validation.js
+++ form-validation.js
@@ -7,22 +7,22 @@ function checkemail() {
   if (re.test(String(email).toLowerCase()) === true) {
     if (lower === email) { // Check if email is lowercase
       form.submit(); // Submit if it's correct
-//     } else {
-//       form.addEventListener('submit', (event) => {
-//         event.preventDefault(); // Prevent form submission
-//         const error = document.createElement('aside'); // Create container for error message
-//         error.textContent = 'Please make sure the email is all lower case'; // Error message
-//         error.id = 'error-popup'; // Add id to style the message
-//         setTimeout(() => {
-//           error.classList.add('fade'); // Add delay of 0.5s to not override class
-//         }, 500);
-//         form.appendChild(error);
-//         setTimeout(() => { // Delay for function to remove appended children
-//           form.removeChild(document.getElementById('error-popup'));
-//         }, 3500);
-//       });
-//     }
-//   }
-// }
+    } else {
+      form.addEventListener('submit', (event) => {
+        event.preventDefault(); // Prevent form submission
+        const error = document.createElement('aside'); // Create container for error message
+        error.textContent = 'Please make sure the email is all lower case'; // Error message
+        error.id = 'error-popup'; // Add id to style the message
+        setTimeout(() => {
+          error.classList.add('fade'); // Add delay of 0.5s to not override class
+        }, 500);
+        form.appendChild(error);
+        setTimeout(() => { // Delay for function to remove appended children
+          form.removeChild(document.getElementById('error-popup'));
+        }, 3500);
+      });
+    }
+  }
+}
 
-// checkemail();
+checkemail();
