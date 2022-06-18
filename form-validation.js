ae1dde5ef3daedadb3ec3b1ae70b46bcaaf624ee
Losalini <c.rokocakau@gmail.com>
2022-06-19 04:58:15 +1200

    Remove some comments from form-validation.js file

--- form-validation.js
+++ form-validation.js
@@ -3,16 +3,16 @@ function checkemail() {
   const email = form.elements.email.value;
   const re = /@/g;
   const lower = email.toLowerCase();
-  // Check if email matches regex for email input
+  
   if (re.test(String(email).toLowerCase()) === true) {
     if (lower === email) { // Check if email is lowercase
-      form.submit(); // Submit if it's correct
+      form.submit(); 
     } else {
       form.addEventListener('submit', (event) => {
-        event.preventDefault(); // Prevent form submission
-        const error = document.createElement('aside'); // Create container for error message
-        error.textContent = 'Please make sure the email is all lower case'; // Error message
-        error.id = 'error-popup'; // Add id to style the message
+        event.preventDefault();
+        const error = document.createElement('aside'); 
+        error.textContent = 'Please make sure the email is all lower case'; 
+        error.id = 'error-popup'; 
         setTimeout(() => {
           error.classList.add('fade'); // Add delay of 0.5s to not override class
         }, 500);
