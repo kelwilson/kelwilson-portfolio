ae51e2f05f5a346c4f375c2deeb17699b42ac84f
kelwilson <kelwilsonachienu@gmail.com>
2022-06-18 17:45:19 +0800

    Add an array to the modal.js file

--- script/modal.js
+++ script/modal.js
@@ -1,63 +1,51 @@
 const cardImages = './img/laptop';
 
 const card1 = {
-  title: 'To Do List',
-  technologies: [
-    'HTML/CSS',
-    'Webpack',
-    'JavaScript',
-    'Jest'
-  ],
-  description: 'Simple to do list, many functions included like drag & drop, double click to edit and more! ',
-  link1: 'https://github.com/ReshyResh/To-Do-List-JS',
-  link2: 'https://reshyresh.github.io/To-Do-List-JS/index.html'
+  title: "project name goes here",
+  technologies: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ullam modi architecto reprehenderit officiis consequatur id molestias sint, maxime cumque ad excepturi atque voluptatem nihil delectus placeat recusandae natus exercitationem!",
+  link1: "https://kelwilson.github.io/kelwilson-portfolio/",
+  link2: "https://github.com/kelwilson",
 };
 
 const card2 = {
-  title: 'Awesome Books',
-  technologies: [
-    'HTML/CSS',
-    'JavaScript',
-  ],
-  description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ullam modi architecto reprehenderit officiis consequatur id molestias sint, maxime cumque ad excepturi atque voluptatem nihil delectus placeat recusandae natus exercitationem!',
-  link1: 'https://github.com/ReshyResh/Awesome-Books-JS-Project',
-  link2: 'https://reshyresh.github.io/Awesome-Books-JS-Project/',
+  title: "project name goes here",
+  technologies: ["HTML/CSS", "JavaScript"],
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ullam modi architecto reprehenderit officiis consequatur id molestias sint, maxime cumque ad excepturi atque voluptatem nihil delectus placeat recusandae natus exercitationem!",
+  link1: "https://kelwilson.github.io/kelwilson-portfolio/",
+  link2: "https://github.com/kelwilson",
 };
 
 const card3 = {
-  title: 'Third Project Name',
-  technologies: [
-    'HTML/CSS',
-    'Ruby on Rails',
-    'JavaScript',
-  ],
-  description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ullam modi architecto reprehenderit officiis consequatur id molestias sint, maxime cumque ad excepturi atque voluptatem nihil delectus placeat recusandae natus exercitationem!',
-  link: 'https://linktothirdproject.com',
+  title: "project name goes here",
+  technologies: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ullam modi architecto reprehenderit officiis consequatur id molestias sint, maxime cumque ad excepturi atque voluptatem nihil delectus placeat recusandae natus exercitationem!",
+  // link1: "https://kelwilson.github.io/kelwilson-portfolio/",
+  // link2: "https://github.com/kelwilson",
 };
 
-// const card4 = {
-//   title: 'Fifth Project Name',
-//   technologies: [
-//     'HTML/CSS',
-//     'Ruby on Rails',
-//     'JavaScript',
-//   ],
-//   description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ullam modi architecto reprehenderit officiis consequatur id molestias sint, maxime cumque ad excepturi atque voluptatem nihil delectus placeat recusandae natus exercitationem!',
-//   link: 'https://linktofifthproject.com',
-// };
+const card4 = {
+  title: "project name goes here",
+  technologies: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ullam modi architecto reprehenderit officiis consequatur id molestias sint, maxime cumque ad excepturi atque voluptatem nihil delectus placeat recusandae natus exercitationem!",
+  // link1: "https://kelwilson.github.io/kelwilson-portfolio/",
+  // link2: "https://github.com/kelwilson",
+};
 
-// const card5 = {
-//   title: 'HTML/CSS Capstone Project',
-//   technologies: [
-//     'HTML/CSS',
-//     'JavaScript',
-//   ],
-//   description: 'Conference-like website used as capstone project for the HTML/CSS module. Includes dynamically loaded content and responsive layouts.',
-//   link1: 'https://github.com/ReshyResh/Capstone-Project-HTML-CSS-JS',
-//   link2: 'https://reshyresh.github.io/Capstone-Project-HTML-CSS-JS/index.html',
-// };
+const card5 = {
+  title: "project name goes here",
+  technologies: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ullam modi architecto reprehenderit officiis consequatur id molestias sint, maxime cumque ad excepturi atque voluptatem nihil delectus placeat recusandae natus exercitationem!",
+  // link1: "https://kelwilson.github.io/kelwilson-portfolio/",
+  // link2: "https://github.com/kelwilson",
+};
 
-// const projects = [card1, card2, card3, card4, card5]; // Array for cards contents
+const projects = [card1, card2, card3, card4, card5]; // Array for cards contents
 
 // function modal(number) { // The argument is given by the button onclick
 //   const card = document.querySelector('#card');
