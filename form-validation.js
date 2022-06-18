23269c5b08a48bab25a19e705b1ba9482f53f07f
Losalini <c.rokocakau@gmail.com>
2022-06-19 05:23:03 +1200

    Fix eslint linter errors within local environment

--- form-validation.js
+++ form-validation.js
@@ -3,16 +3,16 @@ function checkemail() {
   const email = form.elements.email.value;
   const re = /@/g;
   const lower = email.toLowerCase();
-  
+
   if (re.test(String(email).toLowerCase()) === true) {
     if (lower === email) { // Check if email is lowercase
-      form.submit(); 
+      form.submit();
     } else {
       form.addEventListener('submit', (event) => {
         event.preventDefault();
-        const error = document.createElement('aside'); 
-        error.textContent = 'Please make sure the email is all lower case'; 
-        error.id = 'error-popup'; 
+        const error = document.createElement('aside');
+        error.textContent = 'Please make sure the email is all lower case';
+        error.id = 'error-popup';
         setTimeout(() => {
           error.classList.add('fade'); // Add delay of 0.5s to not override class
         }, 500);
